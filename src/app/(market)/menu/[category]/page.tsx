ea0f75d11582375a6c014014c09edf085049d619
Jinsu <injnamek@gmail.com>
2025-03-10 11:46:25 +0900

    feat: 카테고리 페이지 data fetching

--- src/app/(market)/menu/[category]/page.tsx
+++ src/app/(market)/menu/[category]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { fetchApi } from "lib/api";
+
 type CategoryProps = {
   params: {
     category: string;
@@ -9,6 +12,22 @@ export default function Category({ params }: CategoryProps) {
   const { category } = params;
 
   // 카테고리별 데이터 가져오기
+  const { data: products } = useQuery({
+    queryKey: ["products", category],
+    queryFn: async () => {
+      const params = new URLSearchParams();
+      params.append("custom", JSON.stringify({ "extra.category": category }));
+
+      const data = await fetchApi(`/products?${params.toString()}`, {
+        next: {
+          revalidate: 60,
+        },
+      });
+      return data;
+    },
+  });
+
+  console.log(products);
 
   return <h1>{category}</h1>;
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,13 @@
 // RequestInit : fetch API의 기본 옵션 타입
 // HeadersInit : 헤더 관련 타입
 // RequestInit에서 headers 속성만 제외한 타입 + headers 속성을 추가하여 커스텀 타입 정의
-type FetchApiOptions = RequestInit;
+type FetchApiOptions = RequestInit & {
+  next?: {
+    revalidate?: number | false;
+    tags?: string[];
+  };
+  cache?: RequestCache;
+};
 
 // 공통 fetch 유틸리티
 // fetch api처럼 매개변수로 url과 options 객체를 받는다.
@@ -20,9 +26,15 @@ export async function fetchApi(
   // fetchApi 함수를 호출하는 곳에서 전체 url을 전달할 때랑 endpoint만 전달할 때 둘 다 적용되게 하기
   const url = endpoint.startsWith("http") ? endpoint : `${baseUrl}${endpoint}`;
 
+  // Next.js data fetching 관련 옵션 추출
+  const { next, cache, ...restOptions } = options;
+
+  // Next.js 옵션을 fetch에 전달
   const response = await fetch(url, {
-    ...options,
-    headers: headers,
+    ...restOptions,
+    next,
+    cache,
+    headers, // 커스텀 headers를 명시적으로 전달해야 기본 headers와 병합됨.
   });
 
   // 호출할 때는 바로 data를 받을 수 있음
