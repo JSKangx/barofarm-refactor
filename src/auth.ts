20f8765f3843a115062e7b72d28c4f337c9f4103
Jinsu <injnamek@gmail.com>
2025-03-13 11:41:01 +0900

    ci: bcrypt 의존성 설치

--- src/auth.ts
+++ src/auth.ts
@@ -16,13 +16,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: "Credentials",
       // 로그인폼에 표시할 필드를 정의한다.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
+        email: { label: "Email", type: "email", placeholder: "email" },
         password: { label: "Password", type: "password" },
       },
       // authorize : 사용자 자격 증명을 검증하는 함수
       async authorize(credentials): Promise<User | null> {
+        if (!process.env.MONGODB_URI) {
+          throw new Error("MONGODB_URI 환경 변수가 설정되지 않았습니다.");
+        }
+        // 바로팜 MongoDB 가져오기
         const client = await MongoClient.connect(process.env.MONGODB_URI);
         const db = client.db("baroFarm");
+
+        // 유저 찾기 쿼리 수행
+        const user = await db.collection("user").findOne({
+          email: credentials.email,
+        });
 
         return user
           ? { id: user.id, name: user.name, email: user.email }
